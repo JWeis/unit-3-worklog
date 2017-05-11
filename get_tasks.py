51b495e7351ee219108f6bebd411737cc834d86a
James Weis <jweis@revampagency.com>
2017-05-11 15:24:09 -0400

    added functionality to get all available dates to choose from

--- get_tasks.py
+++ get_tasks.py
@@ -10,7 +10,19 @@ class GetTask:
     def print_all(self):
         print(self.rows)
 
+    def available_dates(self):
+        list_available_dates = []
+        for i in range(1, len(self.rows)):
+            list_available_dates.append(self.rows[i][0])
+        return list_available_dates
+
     def by_date(self, date):
         for i in range(1, len(self.rows)):
             if date == self.rows[i][0]:
+                print (self.rows[i])
+
+    def by_time_spent(self, time_spent):
+        time_spent = str(time_spent)
+        for i in range(1, len(self.rows)):
+            if time_spent == self.rows[i][3]:
                 print (self.rows[i])
