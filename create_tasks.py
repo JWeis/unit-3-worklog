d355da83513bc1747286c4761cc0b9ac43ee2d11
James Weis <jweis@revampagency.com>
2017-05-12 14:01:48 -0400

    changed creating tasks by employee and get_tasks by date

--- create_tasks.py
+++ create_tasks.py
@@ -1,19 +1,22 @@
 import csv
 import datetime
 
-class NewTask:
+
+class Task:
     # create new tasks with name, timestamp and notes
-    def __init__(self, task_name, task_notes, time_spent):
-        self.task_name = task_name
-        self.task_notes = task_notes
-        self.time_spent = time_spent
-        self.time_stamp = datetime.datetime.now().strftime('%m/%d/%Y')
-        with open('work_log.csv', 'a') as csvfile:
-            fieldnames = ['Date', 'Task Name', 'Task Notes', 'Time Spent']
-            taskwriter = csv.writer(csvfile, fieldnames=fieldnames)
-            taskwriter.writerow({
-                'Date' : self.time_stamp,
-                'Task Name' : task_name,
-                'Task Notes' : task_notes,
-                'Time Spent' : time_spent
-            })
+    def __init__(self, employee_name):
+        self.employee_name = employee_name
+        self.created_date = datetime.datetime.now().strftime('%m/%d/%Y')
+        self.fieldnames = ['Date', 'Task Name', 'Task Notes', 'Time Spent']
+        with open(employee_name + '_work_log.csv', 'a') as csvfile:
+            self.task_writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
+            self.task_writer.writeheader()
+
+    def new_task(self, task_name, task_notes, time_spent):
+        with open(self.employee_name + '_work_log.csv', 'a') as csvfile:
+            self.task_writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
+            self.task_writer.writerow({
+                'Date': self.created_date,
+                'Task Name': task_name,
+                'Task Notes': task_notes,
+                'Time Spent': time_spent})
--- get_tasks.py
+++ get_tasks.py
@@ -1,28 +1,48 @@
 import csv
-import datetime
+
 
 class GetTask:
-    def __init__(self):
-        self.data = open('work_log.csv', 'r')
-        self.taskreader = csv.reader(self.data)
-        self.rows = list(self.taskreader)
+    def __init__(self, filename):
+        self.filename = filename
+        self.data = open(filename, 'r')
+        self.task_reader = csv.DictReader(self.data, delimiter=',')
+        self.rows = list(self.task_reader)
 
     def print_all(self):
-        print(self.rows)
+        return (self.rows)
 
     def available_dates(self):
-        list_available_dates = []
-        for i in range(1, len(self.rows)):
-            list_available_dates.append(self.rows[i][0])
-        return list_available_dates
+        for row in self.rows[:]:
+            print(row['Date'])
 
     def by_date(self, date):
-        for i in range(1, len(self.rows)):
-            if date == self.rows[i][0]:
-                print (self.rows[i])
+        print("\nTask by date created\n")
+        for row in self.rows[:]:
+            check_date = row['Date']
+            if check_date == date:
+                return row
 
     def by_time_spent(self, time_spent):
+        print("\nTask by time spent\n")
+        print(self.rows[0])
         time_spent = str(time_spent)
         for i in range(1, len(self.rows)):
             if time_spent == self.rows[i][3]:
-                print (self.rows[i])
+                print (self.rows[i], '\n')
+
+    def exact_match(self, string):
+        string_list = string.split()
+        output = []
+        for a in range(len(string_list)):
+            for i in range(len(self.rows)):
+                if string_list[a] in self.rows[i][i]:
+                    output.append(self.rows[i])
+                else: print('no matches')
+        print(output)
+
+james = GetTask("Jim_work_log.csv")
+
+james.available_dates()
+james.by_date('05/12/2017')
+
+#test = james.print_all()
