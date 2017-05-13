a092ee3414b19316ed9b5a4fcfae17a586c9f110
James Weis <jweis@revampagency.com>
2017-05-12 20:35:06 -0400

    working through command line menus

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from create_tasks import Task
 from get_tasks import GetTask
 
+
 def app():
     start = input("Welcome Please select what you would like to do: 1: Add New Employee Work Log, 2: Review Work Log")
     if start == '1':
@@ -16,5 +17,13 @@ def app():
             new_work_log.new_task(task_name, task_notes, time_spent)
     if start == '2':
         # run get_tasks
-        pass
+        work_log = input("What is the name of the employee?  ")
+        emp_csv = work_log+'_work_log.csv'
+        emp_log = GetTask(emp_csv)
+        print("You now have {} work log. ".format(work_log))
+        first_action = input("would you like to get all logs for {}? 1: Yes, 2: No  ".format(work_log))
+        if first_action == '1':
+            print(emp_log.print_all())
+
+
 app()
--- create_tasks.py
+++ create_tasks.py
@@ -6,18 +6,23 @@ class Task:
     # create new tasks with name, timestamp and notes
     def __init__(self, employee_name):
         self.employee_name = employee_name
-        self.created_date = datetime.datetime.now().strftime('%m/%d/%Y')
         self.fieldnames = ['Date', 'Task Name', 'Task Notes', 'Time Spent']
         with open(employee_name + '_work_log.csv', 'a') as csvfile:
             self.task_writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
             self.task_writer.writeheader()
 
     def new_task(self, task_name, task_notes, time_spent):
+        created_date = datetime.datetime.now().strftime('%m/%d/%Y')
         with open(self.employee_name + '_work_log.csv', 'a') as csvfile:
             self.task_writer = csv.DictWriter(csvfile, fieldnames=self.fieldnames)
             self.task_writer.writerow({
-                'Date': self.created_date,
+                'Date': created_date,
                 'Task Name': task_name,
                 'Task Notes': task_notes,
                 'Time Spent': time_spent})
 
+    def update_task(self):
+        pass
+
+    def del_task(self):
+        pass
